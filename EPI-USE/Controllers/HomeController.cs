611851bd0593e92dade8d9ed3921e0dc028d0bf8
ComfyNas <111591592+ComfyNas@users.noreply.github.com>
2024-10-30 01:47:48 +0200

    add gravatar integration

--- EPI-USE/Controllers/HomeController.cs
+++ EPI-USE/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using EPI_USE.Data;
 using EPI_USE.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,14 +10,31 @@ namespace EPI_USE.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var employeeCounts = _context.Employees
+                            .GroupBy(e => e.ManagerId == null)
+                            .Select(g => new
+                            {
+                                HasManager = !g.Key,  // true if employees have a manager, false if they don’t
+                                Count = g.Count()
+                            })
+                            .ToList();
+
+            var employeesWithManagerCount = employeeCounts.FirstOrDefault(e => e.HasManager)?.Count ?? 0;
+            var employeesWithoutManagerCount = employeeCounts.FirstOrDefault(e => !e.HasManager)?.Count ?? 0;
+
+            ViewBag.employeesWithManagerCount = employeesWithManagerCount;
+            ViewBag.employeesWithoutManagerCount = employeesWithoutManagerCount;
+
             return View();
         }
 
---
+++ EPI-USE/Helpers/GravatarHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EPI_USE.Helpers
+{
+    public static class GravatarHelper
+    {
+        public static string GetGravatarUrl(string email, int size = 100)
+        {
+            // Use MD5 hash for email address
+            using (var md5 = MD5.Create())
+            {
+                var emailBytes = Encoding.UTF8.GetBytes(email.Trim().ToLower());
+                var hashBytes = md5.ComputeHash(emailBytes);
+                var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                // Generate Gravatar URL
+                return $"https://www.gravatar.com/avatar/{hash}?s={size}&d=identicon";
+            }
+        }
+    }
+}
